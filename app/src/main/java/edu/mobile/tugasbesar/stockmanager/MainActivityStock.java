6a532f0127e9e121ea1e25bb4c152fc2bdc1d00c
era desti ramayani <eradestirama@gmail.com>
2018-04-30 20:37:24 +0700

    stockmanagernew

---
+++ app/src/main/java/edu/mobile/tugasbesar/stockmanager/MainActivityStock.java
@@ -0,0 +1,13 @@
+package edu.mobile.tugasbesar.stockmanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivityStock extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_stock);
+    }
+}
